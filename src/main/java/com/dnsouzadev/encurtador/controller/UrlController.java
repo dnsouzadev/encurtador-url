50396f5fa0e0a15a5bdd2fc5a8424afb23a006a5
Daniel Souza <workdndsza@gmail.com>
2024-07-01 11:52:24 -0300

    url implementada

--- src/main/java/com/dnsouzadev/encurtador/controller/UrlController.java
+++ src/main/java/com/dnsouzadev/encurtador/controller/UrlController.java
@@ -24,16 +24,16 @@ public class UrlController {
 
     @PostMapping(value = "/shorten-url")
     public ResponseEntity<ShortenUrlResponse> shortenUrl(@RequestBody ShortenUrlRequest request,
-                                           HttpServletRequest servletRequest) {
-        String id;
+                                                         HttpServletRequest servletRequest) {
 
+        String id;
         do {
             id = RandomStringUtils.randomAlphanumeric(5, 10);
         } while (urlRepository.existsById(id));
 
         urlRepository.save(new UrlEntity(id, request.url(), LocalDateTime.now().plusMinutes(1)));
 
-        var redirectUrl = servletRequest.getRequestURL().toString().replace("shorten-url/", id);
+        var redirectUrl = servletRequest.getRequestURL().toString().replace("shorten-url", id);
 
         return ResponseEntity.ok(new ShortenUrlResponse(redirectUrl));
     }
