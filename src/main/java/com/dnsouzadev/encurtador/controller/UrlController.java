663ca0eb40e89848aaeaa75127470b8dc99df566
Daniel Souza <workdndsza@gmail.com>
2024-07-01 11:59:06 -0300

    terminando a api

--- src/main/java/com/dnsouzadev/encurtador/controller/UrlController.java
+++ src/main/java/com/dnsouzadev/encurtador/controller/UrlController.java
@@ -6,11 +6,12 @@ import com.dnsouzadev.encurtador.entities.UrlEntity;
 import com.dnsouzadev.encurtador.repository.UrlRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.time.LocalDateTime;
 
 @RestController
@@ -38,4 +39,18 @@ public class UrlController {
         return ResponseEntity.ok(new ShortenUrlResponse(redirectUrl));
     }
 
+    @GetMapping("{id}")
+    public ResponseEntity<Void> redirect(@PathVariable("id") String id) {
+
+        var url = urlRepository.findById(id);
+
+        if (url.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create(url.get().getFullUrl()));
+
+        return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+    }
 }
